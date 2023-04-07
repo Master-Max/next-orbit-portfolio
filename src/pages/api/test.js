12bdc22a991c66f81c829598577ec4566546b462
mx-laptop <mglxp5@gmail.com>
2023-04-06 20:58:46 -0400

    removed test api route

--- src/pages/api/test.js
+++ src/pages/api/test.js
@@ -1,35 +1,35 @@
 
-import Prospect from "../../models/prospectModel";
-import connectMongo from "../../lib/connectMongo";
+// import Prospect from "../../models/prospectModel";
+// import connectMongo from "../../lib/connectMongo";
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
   
-  if(req.method !==  'POST'){
-    res.status(422).json({error: 'Error'})
-    return;
-  }
+//   if(req.method !==  'POST'){
+//     res.status(422).json({error: 'Error'})
+//     return;
+//   }
 
-  const data = req.body;
+//   const data = req.body;
 
-  console.log(data)
+//   console.log(data)
 
-  const {email, store_name} = data;
+//   const {email, store_name} = data;
 
-  if(!email){
-    res.status(422).json({error: 'Invalid Email'});
-    return;
-  }
+//   if(!email){
+//     res.status(422).json({error: 'Invalid Email'});
+//     return;
+//   }
 
-  console.log(`Attemping to Create Newletter: \n ${email}\n ${store_name}`)
+//   console.log(`Attemping to Create Newletter: \n ${email}\n ${store_name}`)
 
-  await connectMongo();
+//   await connectMongo();
 
-  const newProspect = await new Prospect({
-    email,
-    store_name
-  })
-  newProspect.save()
+//   const newProspect = await new Prospect({
+//     email,
+//     store_name
+//   })
+//   newProspect.save()
 
-  res.status(200).json({ result: JSON.stringify(newProspect), success: true })
-  return;
-}
+//   res.status(200).json({ result: JSON.stringify(newProspect), success: true })
+//   return;
+// }
